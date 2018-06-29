87541ac6817885acf07a0609cd4080f0429dadcf
Nicoara Talpes <nicoaratalpes.com>
2018-06-29 01:45:18 -0700

    Add test for sort order

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+  	@order_delivered_1 = create(:order, is_delivered: true)
+  	@order_delivered_2 = create(:order, is_delivered: true)
+  	@order_pending_1 = create(:order, is_delivered: false)
+  	@order_pending_2 = create(:order, is_delivered: false)
+  end
+
+  test "should get index" do
+    get orders_path
+    assert_response :success
+    assert_select 'li', count: 4
+    assert_select "li[id='#{@order_delivered_1.id.to_s}']"
+    assert_select "li[id='#{@order_delivered_2.id.to_s}']"
+    assert_select "li[id='#{@order_pending_1.id.to_s}']"
+    assert_select "li[id='#{@order_pending_2.id.to_s}']"
+  end
+
+  test "should sort on pending" do
+    get orders_path(:sort => true)
+    assert_response :success
+    assert_select 'li', count: 2
+    assert_select "li[id='#{@order_delivered_1.id.to_s}']"
+    assert_select "li[id='#{@order_delivered_2.id.to_s}']"
+
+    get orders_path(:sort => false)
+    assert_response :success
+    assert_select 'li', count: 2
+    assert_select "li[id='#{@order_pending_1.id.to_s}']"
+    assert_select "li[id='#{@order_pending_2.id.to_s}']"
+  end
 end
